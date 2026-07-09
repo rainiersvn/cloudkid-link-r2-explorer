0374a1c769251ad7918b7d95df1a2b4226375e57
Rainier van Niekerk <rvanniekerk@singular.co.za>
2026-07-09 16:38:37 +0200

    More Auth

--- template/src/index.ts
+++ template/src/index.ts
@@ -3,9 +3,8 @@ import { R2Explorer } from "r2-explorer";
 // Read all available configurations here: https://r2explorer.com/getting-started/configuration/
 export default R2Explorer({
 	readonly: true,
-	cfAccessTeamName: 'cloudkid',
-	basicAuth: {
-		username: "sterben",
-		password: "LordSterben",
-	},
+	// basicAuth: {
+	// 	username: "username",
+	// 	password: "password",
+	// },
 });
